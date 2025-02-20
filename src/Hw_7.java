6332b4b3c7340419aaded6e9eb023917dd8ca9a4
neosome <git@github.com:neosomee/Java.git>
2025-02-20 10:30:38 +0300

    init commit hw7 update

--- src/Hw_7.java
+++ src/Hw_7.java
@@ -2,65 +2,46 @@ public class Hw_7 {
     public static void main(String[] args) {
         int clientOs = 1;
         int clientDeviceYear = 2015;
-        if (clientOs == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOs == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (clientDeviceYear <= 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015) {
+        if (clientOs == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
+        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
 
 
-        int year = 2021;
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println("Год високосный");
-        } else {
-            System.out.println("Год не високосный");
-        }
+            int year = 2021;
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("Год високосный");
+            } else {
+                System.out.println("Год не високосный");
+            }
 
 
-        int deliveryDistance = 95;
-        int deliveryDays;
-        if (deliveryDistance <= 20) {
-            deliveryDays = 1;
-        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            deliveryDays = 2;
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            deliveryDays = 3;
-            System.out.println("Потребуется дней: " + deliveryDays);
-        } else {
-            System.out.println("Свыше 100 км доставки нет.");
-        }
+            int deliveryDistance = 95;
+            int deliveryDays;
+            if (deliveryDistance <= 20) {
+                deliveryDays = 1;
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                deliveryDays = 2;
+            } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                deliveryDays = 3;
+                System.out.println("Потребуется дней: " + deliveryDays);
+            } else {
+                System.out.println("Свыше 100 км доставки нет.");
+            }
 
 
-        int monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-            case 2:
-            case 3:
-                System.out.println("Зима");
-                break;
-            case 4:
-            case 5:
-            case 6:
-                System.out.println("Весна");
-                break;
-            case 7:
-            case 8:
-            case 9:
-                System.out.println("Лето");
-                break;
-            case 10:
-            case 11:
-            case 12:
-                System.out.println("Осень");
-                break;
-            default:
-                System.out.println("Такого месяца не существует");
+            int monthNumber = 12;
+            switch (monthNumber) {
+                case 12, 1, 2 -> System.out.println("Зима");
+                case 3, 4, 5 -> System.out.println("Весна");
+                case 6, 7, 8 -> System.out.println("Лето");
+                case 9, 10, 11 -> System.out.println("Осень");
 
+            }
         }
     }
 }

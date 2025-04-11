c02b8fd2520280d3827d2f85bd988c5f7dbc780a
neosome <167439712+neosomee@users.noreply.github.com>
2025-04-11 14:08:05 +0300

    Update Hw_14.java

--- src/Hw_14.java
+++ src/Hw_14.java
@@ -1,19 +1,16 @@
-public class Hw_14 {
-
-
+public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Aleksander", "Pushkin");
+        Author author2 = new Author("Lev", "Tolstoy");
 
-        Author pushkin = new Author("Aleksandr", "Pushkin");
-        Author tolstoy = new Author("Lev", "Tolstoy");
+        Book book1 = new Book("Eugeniy Onegin", author1, 1823);
+        Book book2 = new Book("War and World", author2, 1863);
 
-        Book onegin = new Book("Evgeni Onegin", "Aleksandr Pushkin", 1831);
-        System.out.println(pushkin.getFirstName() + " " + pushkin.getSurname() + " " + onegin.getName() + " " + onegin.getYear());
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + ", Год: " + book1.getPublicationYear());
+        System.out.println("Книга: " + book2.getTitle() + ", Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() + ", Год: " + book2.getPublicationYear());
 
-        Book warAndWorld = new Book("War and World", "Lev Tolstoy", 1863);
-        System.out.println(tolstoy.getFirstName() + " " + tolstoy.getSurname() + " " + warAndWorld.getName() + " " + warAndWorld.getYear());
-
-        onegin.setYear(1832);
-        System.out.println(pushkin.getFirstName() + " " + pushkin.getSurname() + " " + onegin.getName() + " " + onegin.getYear());
-
+        book1.setPublicationYear(1831);
+        System.out.println("Конец написания книги:");
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + ", Год: " + book1.getPublicationYear());
     }
 }
